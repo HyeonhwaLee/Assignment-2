f367c1f29dff95c2ff3e10092056ae8bacdfb587
HyeonhwaLee <124717511+HyeonhwaLee@users.noreply.github.com>
2023-03-28 17:49:35 +0000

    3/28/2023

--- public/js/faq.js
+++ public/js/faq.js
@@ -2,6 +2,52 @@
 
 const $ = (selector) => document.querySelector(selector);
 
+let timerCounter = 10;
+let timer;
+
+const goToTerms = () => {
+    timerCounter -= 1;
+
+    if(timerCounter>0){
+        $("#seconds").textContent = timerCounter;
+    }else{
+        window.location.href = "terms"
+    }
+}
+
+const acceptTerms = () => {
+    clearInterval(timer);
+    $("#terms").setAttribute("class","hidden");
+}
+
+const toggleQuestion = (evt) => {
+    evt.currentTarget.classList.toggle("minus");
+    let answerDiv = evt.currentTarget.nextElementSibling;
+    answerDiv.classList.toggle("open");
+
+}
+
 document.addEventListener("DOMContentLoaded", () => {
 
+    $("#seconds").textContent = timerCounter;
+    //step1: check if user accepted terms
+    const query = window.location.search;
+    const urlParameters = new URLSearchParams(query);
+    const accepted = urlParameters.get("accepted");
+    //console.log(urlParameters.get("first_name")+ " is my name");
+    // make sure using "&"
+
+    $("#accept").addEventListener("click", acceptTerms);
+
+    const h2Elements = document.querySelectorAll("h2"); //h2 is an element so there's not #
+
+    for(let h2 of h2Elements){
+        h2.addEventListener("click", toggleQuestion);
+    }
+    
+    if(accepted){
+        $("#terms").setAttribute("class","hidden");
+    }else{
+        timer = setInterval(goToTerms, 1000);
+    }
 });
